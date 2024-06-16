da20af74e3365549e90c16ae5958cbd0a4e12038
扣税国王 <170108289+neverforward@users.noreply.github.com>
2024-06-16 17:32:05 +0800

    Add files via upload

---
+++ cpp-win.cpp
@@ -0,0 +1,378 @@
+﻿// cpp-win.cpp : 定义应用程序的入口点。
+//
+
+#include "framework.h"
+#include "cpp-win.h"
+
+#include <cstring>
+
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此处放置代码。
+
+    // 初始化全局字符串
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_CPPWIN, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CPPWIN));
+
+    MSG msg;
+
+    // 主消息循环:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CPPWIN));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_CPPWIN);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+// Main
+INT_PTR CALLBACK Main(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        
+        switch (LOWORD(wParam))
+        {
+        case IDOK:
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        case IDC_BILIBILI:
+            system("start https://bilibili.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_BING:
+            system("start https://bing.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_GITHUB:
+            system("start https://github.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_CPP:
+            system("start https://zh.cppreference.com/w/%E9%A6%96%E9%A1%B5/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_LEETCODE:
+            system("start https://leetcode.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_CSDN:
+            system("start https://www.csdn.net/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_123pan:
+            system("start https://www.123pan.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_BAIDUS:
+            system("start https://www.baidu.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_TRANSLATOR:
+            system("start https://cn.bing.com/translator/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_BAIDUM:
+            system("start https://map.baidu.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_MSLEARN:
+            system("start https://learn.microsoft.com/zh-cn/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_DOUYIN:
+            system("start https://www.douyin.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_CMD:
+            system("start cmd");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_POWERSHELL:
+            system("start powershell");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_EXPLORER:
+            system("start explorer");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_DIALER:
+            system("start dialer");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_NOTEPAD:
+            system("start notepad");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_MSPAINT:
+            system("start mspaint");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_CALC:
+            system("start calc");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_JYSHARE:
+            system("start https://www.jyshare.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_RUNOOB:
+            system("start https://www.runoob.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_WEIBO:
+            system("start https://weibo.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_KUAISHOU:
+            system("start https://www.kuaishou.com/");
+            return (INT_PTR)TRUE;
+            break;
+        case IDC_ITHOME:
+            system("start https://www.ithome.com/");
+            return (INT_PTR)TRUE;
+            break;
+        default:
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+#define OK return(INT_PTR)TRUE;break
+INT_PTR CALLBACK CMDS(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case IDOK:
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        case IDC_CALC:
+            system("start calc");
+            OK;
+        case IDC_CERTMGR:
+            system("start certmgr.msc");
+            OK;
+        case IDC_CHARMAP:
+            system("start charmap");
+            OK;
+        case IDC_CLEANMGR:
+            system("start cleanmgr");
+            OK;
+        case IDC_SQL:
+            system("start cliconfg");
+            OK;
+        case IDC_CMD:
+            system("start cmd");
+            OK;
+        case IDC_compmgmt_msc:
+            system("start compmgmt.msc");
+            OK;
+        case IDC_DCOMCNFG:
+            system("start dcomcnfg");
+            OK;
+        case IDC_devmgmt_msc:
+            system("start devmgmt.msc");
+            OK;
+        case IDC_diskmgmt_msc:
+            system("start diskmgmt.msc");
+            OK;
+        case IDC_dxdiag:
+            system("start dxdiag");
+            OK;
+        case IDC_EXPLORER:
+            system("start explorer");
+            OK;
+        default:
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+            break;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 分析菜单选择:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            case ID_MAIN:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_MAIN), hWnd, Main);
+                break;
+            case ID_shutdown:
+                system("shutdown -s -t 100");
+                break;
+            case ID_noshutdown:
+                system("shutdown -a");
+                break;
+            case ID_power:
+                system("shutdown -i");
+                break;
+            case ID_COMMAND:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_COMMAND), hWnd, CMDS);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // TextOut(hdc, 10, 10, L"打开菜单栏中的main选项，选择功能！", wcslen(L"打开菜单栏中的main选项，选择功能！"));
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ resource.h
@@ -0,0 +1,88 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 cpp-win.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_CPPWIN_DIALOG               102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_CPPWIN                      107
+#define IDI_SMALL                       108
+#define IDC_CPPWIN                      109
+#define IDR_MAINFRAME                   128
+#define IDD_MAIN                        129
+#define IDD_DIALOG1                     133
+#define IDD_COMMAND                     133
+#define IDC_BILIBILI                    1000
+#define IDC_BING                        1001
+#define IDC_GITHUB                      1003
+#define IDC_CPP                         1004
+#define IDC_LEETCODE                    1005
+#define IDC_CSDN                        1006
+#define IDC_123pan                      1007
+#define IDC_BAIDUS                      1008
+#define IDC_TRANSLATOR                  1009
+#define IDC_BAIDUM                      1010
+#define IDC_MSLEARN                     1011
+#define IDC_DOUYIN                      1012
+#define IDC_CMD                         1013
+#define IDC_POWERSHELL                  1014
+#define IDC_PROGRESS1                   1015
+#define IDC_PROGRESS2                   1016
+#define IDC_EXPLORER                    1017
+#define IDC_DIALER                      1018
+#define IDC_NOTEPAD                     1019
+#define IDC_BUTTON8                     1020
+#define IDC_MSPAINT                     1020
+#define IDC_compmgmt_msc                1020
+#define IDC_CALC                        1027
+#define IDC_JYSHARE                     1028
+#define IDC_RUNOOB                      1029
+#define IDC_WEIBO                       1030
+#define IDC_KUAISHOU                    1031
+#define IDC_ITHOME                      1032
+#define IDC_CERTMGR                     1046
+#define IDC_CHARMAP                     1047
+#define IDC_CLEANMGR                    1048
+#define IDC_SQL                         1049
+#define IDC_DCOMCNFG                    1051
+#define IDC_devmgmt_msc                 1052
+#define IDC_diskmgmt_msc                1053
+#define IDC_dxdiag                      1054
+#define ID_MAIN_32771                   32771
+#define ID_MAIN                         32772
+#define ID_MAIN_32773                   32773
+#define ID_TOOLS                        32774
+#define ID_MAIN_2B32775                 32775
+#define ID_2B32776                      32776
+#define ID_2B32777                      32777
+#define ID_2B32778                      32778
+#define ID_CPP                          32779
+#define ID_HTML                         32780
+#define ID_CSS                          32781
+#define ID_MAIN_                        32782
+#define ID_BLUE                         32783
+#define ID_MAIN_32784                   32784
+#define ID_32785                        32785
+#define ID_32786                        32786
+#define ID_shutdown                     32787
+#define ID_noshutdown                   32788
+#define ID_MAIN_32789                   32789
+#define ID_COMMAND                      32790
+#define ID_32791                        32791
+#define ID_power                        32792
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32797
+#define _APS_NEXT_CONTROL_VALUE         1056
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
